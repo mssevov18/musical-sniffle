9a39c11572c147d56537dc106bf02fe4ae706d14
Martin <49585945+mssevov18@users.noreply.github.com>
2022-11-04 10:47:14 +0200

    Initial commit

---
+++ 2223-12-asd-ex-CalculateMagnitude/2223-12-asd-ex-CalculateMagnitude.cpp
@@ -0,0 +1,21 @@
+// 2223-12-asd-ex-CalculateMagnitude.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int calc(int a, int b) {
+    // write your code her
+    return 0;
+}
+
+int main()
+{
+    int a = 5; int b = 3; cout << calc(a, b) << endl;
+    a = 2; b = 3; cout << calc(a, b) << endl;
+    a = 4; b = 4; cout << calc(a, b) << endl;
+    a = 3; b = 8; cout << calc(a, b) << endl;
+    a = 5; b = 5; cout << calc(a, b);
+    
+
+}
